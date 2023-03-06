8ccf98f60adb0a7951d542eefb3da0ab6359099c
Damian Ruiz <nrt.damian@gmail.com>
2023-03-06 16:02:41 -0500

    deploy workflow added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://jondam1985.github.io',
+    base: '/grace-of-god-website-rebuild',
+});
