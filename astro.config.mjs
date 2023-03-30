b58409ca7105936d69487eeed96079fd72c8dae4
Damian Ruiz <nrt.damian@gmail.com>
2023-03-30 18:10:30 -0400

    vite options added

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-
+  vite: {
+    build: {
+        rollupOptions: {
+            external: [
+                "Airtable"
+            ]
+        }
+    }
+  }
 });
