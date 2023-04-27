3e75562c1f5ab9b071bc2119a60c2ff587c141cb
Damian Ruiz <nrt.damian@gmail.com>
2023-04-27 12:40:59 -0400

    config updated cloudflare functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,7 @@ export default defineConfig({
             ]
         }
     }
-  }
+  },
+  output: 'server',
+  adapter: cloudflare({ mode: "directory" }),
 });
