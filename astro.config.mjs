0322a93599f94046acf59233d94329a13eccc0e7
Damian Ruiz <nrt.damian@gmail.com>
2023-04-27 12:45:31 -0400

    cloudflare adapter installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig } from 'astro/config';
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   vite: {
     build: {
-        rollupOptions: {
-            external: [
-                'airtable'
-            ]
-        }
+      rollupOptions: {
+        external: ['airtable']
+      }
     }
   },
   output: 'server',
-  adapter: cloudflare({ mode: "directory" }),
+  adapter: cloudflare()
 });
